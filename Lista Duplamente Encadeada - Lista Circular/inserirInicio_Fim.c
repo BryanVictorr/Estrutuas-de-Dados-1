48780b9a687588d9b1e8e3993813e4b4cd544a0d
Bryan Victor <105087272+BryanVictorr@users.noreply.github.com>
2023-05-09 14:25:57 -0300

    Add files via upload

---
+++ Lista Duplamente Encadeada - Lista Circular/inserirInicio_Fim.c
@@ -0,0 +1,48 @@
+No *inserirInicio(No *lista, char *nome, int idade, int cpf){
+
+    
+
+    No *novo = (No*) malloc(sizeof(No));
+    
+    strcpy(novo->nome, nome);
+    novo->cpf = cpf;
+    novo->idade = idade;
+    
+    if(lista == NULL){
+        
+        novo->anterior = NULL;
+        novo->proximo = NULL;
+    }
+    else{
+
+        novo->proximo = lista;
+        novo->anterior = NULL;
+    }
+    return novo;
+}
+
+No *inserirFim(No *lista, char *nome, int idade, int cpf){
+
+    No *novo = (No*) malloc(sizeof(No));
+
+    strcpy(novo->nome, nome);
+    novo->cpf = cpf;
+    novo->idade = idade;
+
+    if(lista == NULL){
+
+        novo->proximo = NULL;
+        novo->anterior = NULL;
+        return novo;
+    }
+    else{
+
+        No *aux;
+        for(aux = lista; aux->proximo != NULL; aux = aux->proximo);
+
+        novo->anterior = aux;
+        novo->proximo = NULL;
+        aux->proximo = novo;
+        return lista;
+    }
+}
